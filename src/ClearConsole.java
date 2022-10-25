f6ad274c2b0c00f120e8ebaa8accf2129871bc11
DavidRadd <dradabau@amazon.com>
2022-10-25 15:24:42 -0700

    2. added clear screen. AB#446

--- src/ClearConsole.java
+++ src/ClearConsole.java
@@ -1,3 +1,10 @@
-package PACKAGE_NAME;class ClearConsole {
-
+public class ClearConsole {
+    // https://stackoverflow.com/questions/46242330/clearing-console-in-intellij-idea
+    // the intellij terminal is not a console and cannot be cleared.
+    // this should/may clear if run from a jar file
+    public static void clearConsole()
+    {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
