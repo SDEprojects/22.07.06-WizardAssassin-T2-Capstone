08058f0a222512afbd5d20b85de32fa291204377
Kevin Kash <kevin.kash@tlgcohort.com>
2022-11-02 14:00:13 -0700

    Kash_AB#842
    Modified GameWindow and GamePanel.
    
    GamePanel will give the impression of changing screens by implementing methods.
    
    Added function to buttons to navigate screens.
    
    Added StoneWall.jpeg.

--- src/com/wizard_assassin/graphics/GameWindow.java
+++ src/com/wizard_assassin/graphics/GameWindow.java
@@ -1,18 +1,25 @@
 package com.wizard_assassin.graphics;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class GameWindow extends JFrame {
+public class GameWindow {
 
+    private JFrame jFrame;
     // CONSTRUCTOR
-    public GameWindow(GamePanel gamePanel) {
-        JFrame jFrame = new JFrame();
 
+    public GameWindow(GamePanel gamePanel) {
+        initialize(gamePanel);
+    }
+
+    private void initialize(GamePanel gamePanel) {
+        jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
         jFrame.setResizable(false);
         jFrame.pack();
         jFrame.setVisible(true);
+        jFrame.setTitle("Wizard Assassin");
     }
 }
