93bfa064ec01b4cadbb37e312bc101f88857a60d
liangyaliao <liaoliangyan@gmail.com>
2022-11-10 13:54:01 -0500

    Updated music   AB#556

--- src/com/wizard_assassin/model/Music.java
+++ src/com/wizard_assassin/model/Music.java
@@ -1,52 +1,61 @@
 package com.wizard_assassin.model;
 
+import javax.imageio.ImageIO;
 import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.awt.image.BufferedImage;
 
 public class Music {
     private boolean musicOn = true;
-    private JFrame jFrame = new JFrame();
+    private JFrame musicFrame = new JFrame();
     //JPanel musicPanel = new JPanel();
     JButton playButton = new JButton("Play");
     JButton stopButton = new JButton("Stop");
-    JButton volumeUpButton = new JButton("VOLUME+");
-    JButton volumeDownButton = new JButton("VOLUME-");
+    JButton volumeUpButton = new JButton("VOL +");
+    JButton volumeDownButton = new JButton("VOL -");
 
     JLabel musicLabel = new JLabel();
     Clip clip= AudioSystem.getClip();
     float volume = -9.0f;
     FloatControl fc;
 
+    private BufferedImage backgroundImage;
+
     public Music() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
     }
 
     public void initialize() {
-        jFrame.setTitle("Music");
-        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        jFrame.setSize(500, 300);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setBackground(Color.green);
+        musicFrame.setTitle("Music");
+        musicFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        musicFrame.setSize(500, 300);
+        musicFrame.setLocationRelativeTo(null);
+        //musicFrame.setBackground(Color.green);
         //jFrame.pack();
-        jFrame.setVisible(true);
-        jFrame.setLayout(new GridLayout(1, 5));
+        musicFrame.setVisible(true);
+        //musicFrame.setLayout(new GridLayout(1, 5));
+
         //musicPanel.setBackground(Color.pink);
         //jFrame.add(musicPanel);
         //musicPanel.add(playButton);
         //musicPanel.add(stopButton);
 
-        jFrame.add(playButton);
-        jFrame.add(stopButton);
-        jFrame.add(volumeUpButton);
-        jFrame.add(volumeDownButton);
+        musicFrame.add(playButton);
+        musicFrame.add(stopButton);
+        musicFrame.add(volumeUpButton);
+        musicFrame.add(volumeDownButton);
         musicLabel.setText("Welcome to music control panel! ");
-        jFrame.add(musicLabel);
+        musicFrame.add(musicLabel);
         //musicPanel.add(musicLabel);
+        importImg("TitleScreenResources/musicBkgd.png");
+
+
 
         playButton.addActionListener(new ActionListener() {
             @Override
@@ -54,9 +63,8 @@ public class Music {
                 try {
                     clip.close();
                     setMusicOn(true);
-                   // setVolume(0.0f);
                     playMusic();
-                    System.out.println(volume);
+                    System.out.println(getVolume());
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
@@ -74,7 +82,7 @@ public class Music {
             @Override
             public void actionPerformed(ActionEvent e) {
                 turnUpMusic();
-                System.out.println(volume);
+                System.out.println(getVolume());
             }
         });
 
@@ -82,9 +90,27 @@ public class Music {
             @Override
             public void actionPerformed(ActionEvent e) {
                 turnDownMusic();
-                System.out.println(volume);
+                System.out.println(getVolume());
             }
         });
+    }
+
+    private BufferedImage importImg(String file) {
+        InputStream is = getClass().getClassLoader().getResourceAsStream(file);
+
+        try {
+            backgroundImage = ImageIO.read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        musicFrame.repaint();
+        return null;
     }
 
     public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
@@ -105,60 +131,36 @@ public class Music {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (musicOn) {
+                while (isMusicOn()) {
                     clip.start();
                     clip.loop(Clip.LOOP_CONTINUOUSLY);
-                    if (!musicOn) {
+                    if (!isMusicOn()) {
                         clip.stop();
                     }
                 }
             }
         });
         thread.start();
-
-
-//
-//        clip.close();
-//        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-//        InputStream music = classLoader.getResourceAsStream("music.wav");
-//        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(music));
-//        clip.open(audioInputStream);
-//
-//        Thread thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                while (musicOn) {
-//                    clip.start();
-////                    FloatControl fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-////                    fc.setValue(volume); // set volume to 50% to start
-//                    clip.loop(Clip.LOOP_CONTINUOUSLY);
-//                    if (!musicOn) {
-//                        clip.stop();
-//                    }
-//                }
-//            }
-//        });
-//        thread.start();
     }
 
     public void turnUpMusic() {
         volume += 3.0f;
         if (volume > 6.0f) {
-            volume = 6.0f;
+            setVolume(6.0f);
         }
-        FloatControl fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         fc.setValue(getVolume());
-
     }
 
     public void turnDownMusic() {
         volume -= 3.0f;
         if (volume < -80.0f) {
-            volume = -80.0f;
+            setVolume(-80.0f);
         }
         fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         fc.setValue(getVolume());
     }
+
 
     public float getVolume() {
         return volume;
