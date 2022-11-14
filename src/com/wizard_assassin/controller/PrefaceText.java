ee7317a6b02925647c3d0584abdade7f113ea1fc
liangyaliao <liaoliangyan@gmail.com>
2022-11-14 14:37:34 -0500

    Create prefaceText.json, PrefaceText.java, and modified on GamePanel.java, so prefaceTest can be modified as an external file. AB#979.

---
+++ src/com/wizard_assassin/controller/PrefaceText.java
@@ -0,0 +1,12 @@
+package com.wizard_assassin.controller;
+
+public class PrefaceText {
+private String text;
+
+    public PrefaceText() {
+    }
+
+    public String getText() {
+        return text;
+    }
+}
